64940f3aa3cf60ca40577f60c704c901f521831a
muzaffarsharapov <sharapov0140@gmail.com>
2016-02-01 16:06:30 -0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Zaf on 02/01/2016.
+//  Copyright © 2016 Muzaffar Sharapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
